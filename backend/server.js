b2b770ef06b6c0fbd398a1fd07afe9a0e60b5bbd
Pratham Chaudhari <prathamchaudhari124@gmail.com>
2026-06-24 19:50:38 +0530

    vercel files added

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
